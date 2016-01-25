0fba7f852ceac74b5dcd7ecdcbd682a4294e759c
sunny chao <schaosti@gmail.com>
2016-01-24 19:27:56 -0500

    first commit

---
+++ scripts/controllers.js
@@ -0,0 +1,50 @@
+var storeApp = angular.module('storeApp', ['ngRoute']);
+
+
+storeApp.config(function($routeProvider){
+	$routeProvider
+		.when('/index', {
+			templateUrl: 'apple.html',
+			controller: 'appleController',
+			controllerAs: 'appleCont'
+		})
+		.when('/book', {
+			templateUrl: 'book.html',
+			controller: 'bookController',
+			controllerAs: 'bookCont'
+		})
+		.when('/car', {
+			templateUrl: 'car.html',
+			controller: 'carController',
+			controllerAs: 'carCont'
+		});
+
+});
+
+
+storeApp.controller('appleController',['$scope', function($scope){
+		$scope.items= [
+			{	'name': 'apple',
+				'color': 'red',
+				'price': '$1'
+			}
+		]	
+}]);
+storeApp.controller('bookController', ['$scope', function($scope){
+		// $scope.items= [
+		// 	{	name: 'book',
+		// 		color: 'green',
+		// 		price: '$5'
+		// 	}
+		// ]
+		$scope.name = 'book';
+}]);
+storeApp.controller('carController', ['$scope', function($scope){
+		$scope.items= [
+			{
+				'name': 'car',
+				'color': 'blue',
+				'price': '$5000'
+			}
+		]
+}]);
